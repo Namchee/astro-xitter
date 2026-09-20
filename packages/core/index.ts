4a556d0b07e11acbc8f5dea1106805b0024653f6
Cristopher <cristophernamchee12@gmail.com>
2026-09-20 14:02:38 +0700

    feat: add draggable prop to images

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,3 +1,5 @@
 /* eslint-disable unicorn/no-barrel-files */
 // @ts-ignore just export, trust
 export { default as Xitter } from './src/Xitter.astro';
+export { default as XitterLoading } from './src/XitterLoading.astro';
+
