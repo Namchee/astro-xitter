7f5b39dc284309f50ee74f057c7b93a951972c82
Cristopher <cristophernamchee12@gmail.com>
2026-09-20 22:01:20 +0700

    refactor: migrate docs to root workspace

---
+++ docs/astro.config.mjs
@@ -0,0 +1,32 @@
+import node from '@astrojs/node';
+import starlight from '@astrojs/starlight';
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: node({ mode: 'standalone' }),
+  integrations: [
+    starlight({
+      title: 'Astro Xitter',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Namchee/astro-xitter' }],
+      sidebar: [
+        'getting-started',
+        {
+          label: 'Guides',
+          items: [{ autogenerate: { directory: 'guides' } }],
+        },
+        {
+          label: 'Demo',
+          items: [
+            { autogenerate: { directory: 'demo' } },
+            {
+              label: 'On Demand',
+              link: '/demo/tweet',
+            },
+          ],
+        },
+      ],
+    }),
+  ],
+});
