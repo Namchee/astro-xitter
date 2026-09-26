1137b258efd512c32aa03d6240fd2ef063638eb0
Cristopher <cristophernamchee12@gmail.com>
2026-09-26 13:53:54 +0700

    chore: opt-out session

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({ prerenderEnvironment: 'node' }),
+  session: false,
   integrations: [
     starlight({
       title: 'Astro Xitter',
